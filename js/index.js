4d5d720f187d8b5459401e5ae84a432cb2794475
Chandrika <gehini1302@gmail.com>
2021-06-27 14:44:21 +0530

    updated code to display and close modal forms

--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
 // added functionality to create button
-var linkModal = document.getElementsByClassName("signup-link")[0].onclick = function () { //when user clicks the sign-up link 
-    modal.style.display = "block";//open the sign-up model
-    modal1.style.display = "none";//closes the sign-in model
-  }
   var modal2 = document.getElementById("myModal-create");
   var btn2 = document.getElementById("btn-create");
   var span2 = document.getElementsByClassName("close")[2];
